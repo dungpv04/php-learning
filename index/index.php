edbf9f4e4a07fc11aead3321ed5cd35e486d74c0
vandu <vandu@DESKTOP-CKTBGDM>
2024-11-19 17:53:26 +0700

    adding base layout and dividing components

--- index/index.php
+++ index/index.php
@@ -1,15 +1,73 @@
 <?php include 'header.php'; ?>
-<?php include 'products.php'; ?>
+<?php include 'users.php'; ?>
+
 <main>
- <h2>Danh Sách Sản Phẩm</h2>
-<?php if (empty($products)): ?>
- <p>Không có sản phẩm nào.</p>
-<?php else: ?>
- <ul>
-<?php foreach ($products as $product): ?>
- <li><?= htmlspecialchars($product['name']) ?>: <?= htmlspecialchars($product['price']) ?> VND</li>
-<?php endforeach; ?>
- </ul>
-<?php endif; ?>
+    <h2>Danh Sách Người Dùng</h2>
+
+    <?php if (empty($users)): ?>
+        <p>Không có sản phẩm nào.</p>
+    <?php else: ?>
+        <div class="container-xl">
+            <div class="table-responsive">
+                <div class="table-wrapper">
+                    <div class="table-title">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h2>Quản lí <b>Người dùng</b></h2>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">
+                                    <i class="material-icons">&#xE147;</i> 
+                                    <span>Thêm</span>
+                                </a>
+                                <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal">
+                                    <i class="material-icons">&#xE15C;</i> 
+                                    <span>Xóa</span>
+                                </a>                        
+                            </div>
+                        </div>
+                    </div>
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <span class="custom-checkbox">
+                                        <input type="checkbox" id="selectAll">
+                                        <label for="selectAll"></label>
+                                    </span>
+                                </th>
+                                <th>Họ tên</th>
+                                <th>Email</th>
+                                <th>Địa chỉ</th>
+                                <th>Số điện thoại</th>
+                                <th>Hành động</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($users as $user): ?>
+                                <tr>
+                                    <td>
+                                        <span class="custom-checkbox">
+                                            <input type="checkbox" id="checkbox1" name="options[]" value="1">
+                                            <label for="checkbox1"></label>
+                                        </span>
+                                    </td>
+                                    <td><?= htmlspecialchars($user['name']) ?></td>
+                                    <td><?= htmlspecialchars($user['email']) ?></td>
+                                    <td><?= htmlspecialchars($user['address']) ?></td>
+                                    <td><?= htmlspecialchars($user['phone']) ?></td>
+                                    <td>
+                                        <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                                        <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>        
+        </div>
+    <?php endif; ?>
 </main>
+
 <?php include 'footer.php'; ?>
---
+++ index/users.php
@@ -0,0 +1,34 @@
+<?php
+$users = [
+    [
+        "name" => "Thomas Hardy",
+        "email" => "thomas.hardy89@example.com",
+        "address" => "89 Chiaroscuro Rd, Portland, USA",
+        "phone" => "(171) 555-2222"
+    ],
+    [
+        "name" => "Dominique Perrier",
+        "email" => "dominique.perrier57@example.com",
+        "address" => "Obere Str. 57, Berlin, Germany",
+        "phone" => "(313) 555-5735"
+    ],
+    [
+        "name" => "Maria Anders",
+        "email" => "maria.anders25@example.com",
+        "address" => "25, rue Lauriston, Paris, France",
+        "phone" => "(503) 555-9931"
+    ],
+    [
+        "name" => "Fran Wilson",
+        "email" => "fran.wilson67@example.com",
+        "address" => "C/ Araquil, 67, Madrid, Spain",
+        "phone" => "(204) 619-5731"
+    ],
+    [
+        "name" => "Martin Blank",
+        "email" => "martin.blank34@example.com",
+        "address" => "Via Monte Bianco 34, Turin, Italy",
+        "phone" => "(480) 631-2097"
+    ]
+];
+?>
