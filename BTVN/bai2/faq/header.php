0ab8ca68921bb603a0d38e8441c0956b85f416aa
vandu <vandu@DESKTOP-CKTBGDM>
2024-11-28 16:25:21 +0700

    edit css path

--- BTVN/bai2/faq/header.php
+++ BTVN/bai2/faq/header.php
@@ -3,6 +3,6 @@
 <head>
  <title>Trang Chủ</title>
  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- <link rel="stylesheet" href="/css/style.css">
+ <link rel="stylesheet" href="../css/style.css">
 </head>
 
