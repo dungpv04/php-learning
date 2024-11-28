7052776b7734409c6fcb2bf81fe5f51395a92251
vandu <vandu@DESKTOP-CKTBGDM>
2024-11-28 22:20:05 +0700

    completed ex1

---
+++ BTTH/day1/ex1/flowers.php
@@ -0,0 +1,42 @@
+<?php
+    $flowers = [
+        [
+            "name" => "Đỗ quyên",
+            "description" => "Hoa đỗ quyên là biểu tượng của sự dịu dàng và thanh lịch, 
+                            thường được ví với nét đẹp của người phụ nữ Việt Nam. Đỗ quyên còn mang ý nghĩa về sự kiên cường, 
+                            vì hoa nở rộ ngay cả trong điều kiện khắc nghiệt. 
+                            Loài hoa này thường được trồng để làm cảnh và tạo không gian tươi mới cho ngôi nhà. 
+                            Đỗ quyên cũng mang đến thông điệp về sự sum họp, gắn kết trong gia đình.",
+            "image"=> "images/doquyen.jpg",
+        ],
+        [
+            "name" => "Hải đường",
+            "description" => "Hoa hải đường là loài hoa quý phái, 
+                            mang ý nghĩa giàu sang và phú quý. Vào dịp Tết Nguyên Đán, 
+                            hoa hải đường thường được trưng bày để cầu mong một năm mới đầy tài lộc và hạnh phúc. 
+                            Sắc hoa đỏ hồng rực rỡ của hải đường còn thể hiện sự vui vẻ, ấm áp và tình yêu gia đình. 
+                            Đây cũng là loài hoa được yêu thích trong các dịp lễ hội và trang trí không gian sống.",
+            "image"=> "images/haiduong.jpg",
+        ],
+        [
+            "name" => "Mai",
+            "description" => "Hoa mai là biểu tượng không thể thiếu của mùa xuân, 
+                            đặc biệt ở miền Nam Việt Nam. Sắc vàng của hoa mai tượng trưng cho sự thịnh vượng, 
+                            giàu sang và những điều may mắn trong năm mới. Mỗi cành mai nở rộ là lời chúc phúc, 
+                            mang niềm vui và sự khởi đầu tốt đẹp. Mai cũng thể hiện tinh thần vượt khó, 
+                            luôn mạnh mẽ và kiên cường trong cuộc sống.",
+            "image"=> "images/mai.jpg",
+        ],
+        [
+            "name" => "Tường vy",
+            "description" => "Hoa tường vy mang nét đẹp dịu dàng và trong sáng, 
+                            thường được ví với tuổi trẻ đầy nhiệt huyết. 
+                            Loài hoa này là biểu tượng của tình yêu nhẹ nhàng, 
+                            không phô trương nhưng lại bền lâu. Sắc hoa hồng phấn của tường vy tạo nên cảm giác tươi mới, 
+                            thuần khiết và lãng mạn. Tường vy còn được trồng để tô điểm cho khu vườn, 
+                            đem lại không gian yên bình và thư giãn.",
+            "image"=> "images/tuongvy.jpg",
+        ]
+    ];
+    
+?>
