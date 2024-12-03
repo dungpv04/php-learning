14b63754a3bbc490d2d73f29496ca24cb7d5cf74
vandu <vandu@DESKTOP-CKTBGDM>
2024-12-04 00:35:36 +0700

    remove nabar action (unused)

--- BTVN/bai3/views/user/navigation.php
+++ BTVN/bai3/views/user/navigation.php
@@ -1,6 +1,6 @@
 <div class="navbar">
-        <a href="../home">Trang chủ</a>
-        <a href="../user">Người Dùng</a>
-        <a href="../posts">Bài Viết</a>
-        <a href="../faq">Hỏi đáp</a>
+        <a >Trang chủ</a>
+        <a >Người Dùng</a>
+        <a >Bài Viết</a>
+        <a >Hỏi đáp</a>
 </div>
